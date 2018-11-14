9990ba7dbf3eba6c8b3aabb92363928ff76b8337
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-11-14 16:39:07 +0100

    Updates

---
+++ precache-manifest.0a1dbf7eae5af9169cc22cbdd588f826.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f00d82f72247002bd2afa927eec5f8",
+    "url": "/cover-letter/static/media/thinking-cooperatives.92f00d82.png"
+  },
+  {
+    "revision": "81527affd16e8b7f6c72",
+    "url": "/cover-letter/static/css/main.1f87bed2.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/cover-letter/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "1e8752726513d44a8996",
+    "url": "/cover-letter/static/js/1.1e875272.chunk.js"
+  },
+  {
+    "revision": "611e9083f647a937ab21",
+    "url": "/cover-letter/static/js/runtime~main.611e9083.js"
+  },
+  {
+    "revision": "e058a5cdb0d8b895df6d9ce4cc3c99d7",
+    "url": "/cover-letter/static/media/krystyna-dark.e058a5cd.jpg"
+  },
+  {
+    "revision": "c3721f503f9554b7135c406d1002e751",
+    "url": "/cover-letter/static/media/habitat-old.c3721f50.png"
+  },
+  {
+    "revision": "0703928a3f60bbdaf492c510369a05a6",
+    "url": "/cover-letter/static/media/habitat-new.0703928a.png"
+  },
+  {
+    "revision": "81527affd16e8b7f6c72",
+    "url": "/cover-letter/static/js/main.81527aff.chunk.js"
+  },
+  {
+    "revision": "08ca0809fcba3f11f791f615df429aa6",
+    "url": "/cover-letter/static/media/garfield.08ca0809.gif"
+  },
+  {
+    "revision": "76d0590be01f4b9d8880d86fbbc5cca7",
+    "url": "/cover-letter/static/media/portfolio.76d0590b.png"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/cover-letter/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/cover-letter/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/cover-letter/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/cover-letter/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "1e8752726513d44a8996",
+    "url": "/cover-letter/static/css/1.4ed307af.chunk.css"
+  },
+  {
+    "revision": "5f0896c441fbf5c88ae1288590cb0c1c",
+    "url": "/cover-letter/index.html"
+  }
+];
